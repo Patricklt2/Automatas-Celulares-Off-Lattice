3f3ef5ad25404d3f7a83ef26e51885afeb314c64
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2025-08-13 01:53:12 -0300

    Update Main.java
    Co-Authored-By: martuschvartz <113250694+martuschvartz@users.noreply.github.com>

--- simulation/src/Main.java
+++ simulation/src/Main.java
@@ -4,14 +4,14 @@
 * */
 public class Main {
     public static void main(String[] args) {
-        Simulation sim = new Simulation(5000, 1, 10, 5, 1, 10, "particle_test6969.txt");
-        
+        Simulation sim = new Simulation(300, 1, 100, 25, 1, 0.1, "particle_test694206942069420.txt");
+
         /*
         * Tested with:
         *
         * N=10, L = 5, ts = 1, maxiter = 10, r=1 , nu = 0.1   --> low noise
         * N = 5000, L= 5, ts = 1, maxiter = 10, r = 1, nu = 10   --> high noise
-        *
+        * TODO: WITH 300 1 100 25 1 0.1 , THE VALUES OSCILATE A LOT
         * */
         sim.runSimulation();
     }
