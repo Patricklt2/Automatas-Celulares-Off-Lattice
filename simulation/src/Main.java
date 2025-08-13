aef483179610e09b31a202dc39d46f26b1fbefac
martuschvartz <schvartzmartina@gmail.com>
2025-08-13 12:26:33 -0300

    testing cases

--- simulation/src/Main.java
+++ simulation/src/Main.java
@@ -4,7 +4,7 @@
 * */
 public class Main {
     public static void main(String[] args) {
-        Simulation sim = new Simulation(300, 1, 100, 25, 1, 0.1, "really_final_particle_test694206942069420.txt");
+        Simulation sim = new Simulation(400, 1, 100, 10, 1, 0.1, "fig2a-2.txt");
 
         /*
         * Tested with:
@@ -12,6 +12,12 @@ public class Main {
         * N=10, L = 5, ts = 1, maxiter = 10, r=1 , nu = 0.1   --> low noise
         * N = 5000, L= 5, ts = 1, maxiter = 10, r = 1, nu = 10   --> high noise
         * TODO: WITH 300 1 100 25 1 0.1 , THE VALUES OSCILATE A LOT even with modulo fix
+        *
+        * for fig2a:
+        * N = 400 , step = 1 , macIter = 100 , L = 10 , r = 1 , nu = 5     -> va -> 0
+        *
+        * for fig2a-2:
+        * N = 400 , step = 1 , macIter = 100 , L = 10 , r = 1 , nu = 0,1     -> va -> 1
         * */
         sim.runSimulation();
     }
